a2a06bc85d58abdd6129f90fa670d2584a110caf
JoseTorres022 <joseangel.tosa@outlook.com>
2022-10-27 08:15:16 -0700

    practica 1 introduccion a php

---
+++ holamundo.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    //utilizando el include
+    include("");
+    echo "<br>Hola Mundo", "Hola mundo dos", "hola mundo tres</br>";
+    print "hola mundo";
+    print "hola mundo";
+    $clase = "clase-h1-prueba";
+    $a = 1;
+    if($a==1){
+        echo "<br><h1> styel=background-color: red >si</b>"
+    }
+    ?>
+</body>
+
+</html>
